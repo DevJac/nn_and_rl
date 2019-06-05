edaafdc5b80822ccee14ce1ffeaaf6e86b48dd8a
Devin Jacobs <dbuttonz@gmail.com>
2019-06-05 14:58:53 -0600

    lunar_lander.jl prototype complete

--- lunar_lander.jl
+++ lunar_lander.jl
@@ -2,12 +2,13 @@ using Flux
 using OpenAIGym
 using Printf: @printf
 using PyCall
+import Reinforce: action
 
 const env = GymEnv(:LunarLander, :v2)
 
 const actions = 0:3
 
-const hidden_layer_size = 100
+const hidden_layer_size = 400
 
 const q_model = Chain(
     Dense(8 + 1, hidden_layer_size, relu),
@@ -17,37 +18,50 @@ loss(x, y) = Flux.mse(q_model(x), y) + 0.001 * sum(StatsBase.norm, Flux.params(q
 
 optimizer = NADAM()
 
-function train(epochs)
+function train(sars, epochs; verbose=false)
     x = to_q_x(sars)
     y = to_q_y(sars)
-    @printf(
-        "Epoch %4d / %4d - Loss: %8.4f\n",
-        0,
-        epochs,
-        loss(x, y).data)
-    for epoch in 1:epochs
-        Flux.train!(loss, Flux.params(q_model), [(x, y)], optimizer)
+    if verbose
         @printf(
             "Epoch %4d / %4d - Loss: %8.4f\n",
-            epoch,
+            0,
             epochs,
             loss(x, y).data)
     end
+    for epoch in 1:epochs
+        Flux.train!(loss, Flux.params(q_model), [(x, y)], optimizer)
+        if verbose
+            @printf(
+                "Epoch %4d / %4d - Loss: %8.4f\n",
+                epoch,
+                epochs,
+                loss(x, y).data)
+        end
+    end
 end
 
-const sars = Tuple{PyCall.PyArray{Float32,1},Int64,Float64,PyCall.PyArray{Float32,1}}[]
+struct QPolicy <: AbstractPolicy
+    e
+end
+function action(policy::QPolicy, r, s, A)
+    if rand() < policy.e
+        return rand(actions)
+    end
+    argmax(action_values(s)) - 1
+end
 
-function run_episodes(n_episodes)
+function run_episodes(n_episodes, policy)
+    sars = Tuple{PyCall.PyArray{Float32,1},Int64,Float64,PyCall.PyArray{Float32,1}}[]
     rewards = Float64[]
     for episode in 1:n_episodes
-        reward = run_episode(env, RandomPolicy()) do (s, a, r, s_next)
+        reward = run_episode(env, policy) do (s, a, r, s_next)
             push!(sars, (s, a, r, s_next))
             render(env)
         end
         push!(rewards, reward)
     end
     close(env)
-    rewards
+    sars, rewards
 end
 
 function to_q_x(sars::AbstractArray{Tuple{PyCall.PyArray{Float32,1},Int64,Float64,PyCall.PyArray{Float32,1}}})
@@ -63,12 +77,13 @@ function to_q_x(sas)
     hcat(xs...)
 end
 
-function V(s)
-    action_values = map(actions) do a
+function action_values(s)
+    map(actions) do a
         q_model(to_q_x([(s, a)]))[1].data
     end
-    maximum(action_values)
 end
+
+V(s) = maximum(action_values(s))
 
 function to_q_y(sars::AbstractArray{Tuple{PyCall.PyArray{Float32,1},Int64,Float64,PyCall.PyArray{Float32,1}}})
     to_q_y((r, s_next) for (_, _, r, s_next) in sars)
@@ -79,3 +94,19 @@ function to_q_y(rss)
         r + 0.9 * V(s_next)
     end
 end
+
+truncate(a, n) = a[max(1, end-n+1):end]
+
+function run()
+    sars = Tuple{PyCall.PyArray{Float32,1},Int64,Float64,PyCall.PyArray{Float32,1}}[]
+    for i in 1:1000
+        new_sars, rewards = run_episodes(10, QPolicy(0.2))
+        sars = truncate(vcat(sars, new_sars), 1000)
+        @printf("Average rewards: %8.2f\n", sum(rewards) / length(rewards))
+        for _ in 1:10
+            train(sars, 10)
+        end
+    end
+end
+
+run()
